35061d68b72e565c79cc5e6378f50b643e8c2c7f
seanbuckle <seanlbuckle@gmail.com>
2024-09-19 09:04:29 +0100

    add sort and order filters

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSearchParams, Link } from "react-router-dom";
 import { getArticles } from "../api";
+import Filter from "./Filter";
 import NotFound from "./NotFound";
 
 function Home() {
@@ -8,6 +9,8 @@ function Home() {
   const [isError, setIsError] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
   const topicQuery = searchParams.get("topic");
+  const sortByQuery = searchParams.get("sort_by");
+  const orderQuery = searchParams.get("order");
   const dateConfig = {
     day: "numeric",
     month: "long",
@@ -18,7 +21,7 @@ function Home() {
   };
 
   useEffect(() => {
-    getArticles(topicQuery)
+    getArticles(topicQuery, sortByQuery, orderQuery)
       .then((articles) => {
         setAllArticles(articles);
         setIsError(false);
@@ -26,7 +29,7 @@ function Home() {
       .catch((err) => {
         setIsError(true);
       });
-  }, [topicQuery]);
+  }, [topicQuery, sortByQuery, orderQuery]);
 
   return (
     <main>
@@ -35,33 +38,39 @@ function Home() {
           <NotFound />
         </>
       ) : (
-        allArticles.map((article, i) => {
-          const date = new Date(article.created_at);
-          const linkURL = `/${article.article_id}`;
-          return (
-            <Link key={i} to={linkURL}>
-              <section className="ncn-card">
-                <img
-                  className="ncn-card__img"
-                  src={article.article_img_url}
-                  alt="article image"
-                />
-                <h2 className="ncn-card__title">{article.title}</h2>
-                <p className="ncn-card__topic">{article.topic}</p>
-                <time
-                  dateTime={date.toLocaleString("en-GB")}
-                  className="ncn-card__time-date"
-                >
-                  {date.toLocaleTimeString("en-GB", timeConfig)}{" "}
-                  {date.toLocaleString("en-GB", dateConfig)}{" "}
-                  {date.getFullYear() < new Date().getFullYear()
-                    ? date.getFullYear()
-                    : null}
-                </time>
-              </section>
-            </Link>
-          );
-        })
+        <>
+          <Filter
+            searchParams={searchParams}
+            setSearchParams={setSearchParams}
+          />
+          {allArticles.map((article, i) => {
+            const date = new Date(article.created_at);
+            const linkURL = `/${article.article_id}`;
+            return (
+              <Link key={i} to={linkURL}>
+                <section className="ncn-card">
+                  <img
+                    className="ncn-card__img"
+                    src={article.article_img_url}
+                    alt="article image"
+                  />
+                  <h2 className="ncn-card__title">{article.title}</h2>
+                  <p className="ncn-card__topic">{article.topic}</p>
+                  <time
+                    dateTime={date.toLocaleString("en-GB")}
+                    className="ncn-card__time-date"
+                  >
+                    {date.toLocaleTimeString("en-GB", timeConfig)}{" "}
+                    {date.toLocaleString("en-GB", dateConfig)}{" "}
+                    {date.getFullYear() < new Date().getFullYear()
+                      ? date.getFullYear()
+                      : null}
+                  </time>
+                </section>
+              </Link>
+            );
+          })}
+        </>
       )}
     </main>
   );
